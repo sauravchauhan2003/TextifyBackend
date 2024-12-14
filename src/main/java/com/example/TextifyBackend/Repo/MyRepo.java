b1ed0ace11b5e4b15ca4589d51386100f7d708b9
Saurav <sauravchauhan652003@gmail.com>
2024-12-14 23:12:34 +0530

    Revert "First commit"
    This reverts commit 76d841d24519efaa156e6bade34facd559325ca9.

--- src/main/java/com/example/TextifyBackend/Repo/MyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.TextifyBackend.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MyRepo extends JpaRepository<MyUser,Integer> {
-    public Optional<MyUser> findByUsername(String username);
-}
